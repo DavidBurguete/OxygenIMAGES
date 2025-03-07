8e40830dd0806d78b39402058da5d6629f803865
DavidBurguete <dburgueteg@gmail.com>
2025-03-07 20:48:37 +0100

    Done modal and changed library to display images for better looks. Minor changes in Paginate

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,56 +1,107 @@
 import { useTheme } from "./ThemeProvider";
 import { saveAs } from 'file-saver';
 import Masonry from 'react-masonry-css'
+import { ColumnsPhotoAlbum } from "react-photo-album";
+import "react-photo-album/styles.css";
 
 function ImageCard({images}){
     const { isDark } = useTheme();
 
-    const breakpointColumnsObj = {
-        default: 4,
-        999: 2
-    };
+    const gallery = Object.keys(images).map(key => {
+        const imageUrl = images[key].download+"&client_id=Zws9gh0kAQ4lmheTh2imCNYIzl0ZpQYd6HqFDJS0XrI";
+        const imageID = images[key].id;
 
-    return (<Masonry
-                breakpointCols={breakpointColumnsObj}
-                className="image-cards-grid"
-                columnClassName="image-cards"
-            >{
-            Object.keys(images).map(key =>{ 
-                const imageUrl = images[key].download+"&client_id=Zws9gh0kAQ4lmheTh2imCNYIzl0ZpQYd6HqFDJS0XrI";
-
-                async function downloadImage() {
-                    try {
-                        const response = await fetch(imageUrl);
-                        
-                        if (!response.ok) {
-                            throw new Error('Couldn\'t fetch the image');
-                        }
-
-                        const image = await response.json();
-                        
-                        saveAs(image.url, images[key].id+".png");
-                    } catch (error) {
-                        console.error('Error al descargar la imagen:', error);
-                    }
+        async function downloadImage() {
+            try {
+                const response = await fetch(imageUrl);
+                
+                if (!response.ok) {
+                    throw new Error('Couldn\'t fetch the image');
                 }
 
-                return <div className="image-card" key={images[key].id}>
-                    <div className="image-card__card image-card__interact">
-                        <button className={"image-card--save image-card--save--"+(isDark?"dark":"light")}>
-                            <img src={"assets/img/bookmark-"+(isDark?"dark":"light")+".png"} alt="bookmark"></img>
-                        </button>
-                        <div className="image-card__card--download">
-                            <button onClick={downloadImage}><img src="assets/img/download-dark.png" alt=""/></button>
-                        </div>
-                        <img className="image-card__card no-visibility" src={images[key].link}/>
-                    </div>
-                    <img className="image-card__card" src={images[key].link}/>
-                </div>
-            })
+                const image = await response.json();
+                
+                saveAs(image.url, imageID+".png");
+            } catch (error) {
+                console.error('Error al descargar la imagen:', error);
+            }
         }
-    </Masonry>);
+        
+        function fadeIn(){
+            document.getElementsByTagName("html")[0].style.overflow = "hidden";
+            document.getElementById(imageID).classList.add("modal--fadeIn");
+            let escape = (e) => {
+                if(e["key"] == "Escape"){
+                    closeModal();
+                }
+            }
+            document.addEventListener("keydown", escape);
+        };
+
+        function closeModal(){
+            document.getElementsByTagName("html")[0].style.overflow = "visible";
+            document.getElementById(imageID).classList.remove("modal--fadeIn");
+        }
+
+        const clickOut = (parentNode) => {
+            if(parentNode.target.id === imageID){
+                closeModal();
+            }
+        };
+
+        return {
+            id: images[key].id,
+            src: images[key].link,
+            width: images[key].width,
+            height: images[key].height,
+            download: downloadImage,
+            fadeIn: fadeIn,
+            close: closeModal,
+            clickOut: clickOut
+        }
+    });
+
+    return <ColumnsPhotoAlbum
+        photos={gallery} 
+        render={{
+            photo: ({_},{photo}) => {
+                return <span key={photo.id}>
+                    <div className="image-card" htmlFor={photo.id}>
+                        <div className="image-card__card image-card__interact">
+                            <button className={"image-card--save image-card--save--"+(isDark?"dark":"light")}>
+                                <img src={"assets/img/bookmark-"+(isDark?"dark":"light")+".png"} alt="bookmark"></img>
+                            </button>
+                            <div className="image-card__card--download">
+                                <button onClick={photo.download}>
+                                    <img src="assets/img/download-dark.png" alt="download image"/>
+                                </button>
+                            </div>
+                            <img className="image-card__card no-visibility" src={photo.src} /*alt={images[key].alt}*/ onClick={photo.fadeIn}/>
+                        </div>
+                        <img className="image-card__card" src={photo.src}/* alt={images[key].alt}*//>
+                    </div>
+                    <div className="modal" id={photo.id} role="dialog" aria-labelledby="newsletter" onClick={photo.clickOut}>
+                        <div className="modal-dialog" role="document">
+                            <div className="modal-header">
+                                <button id="close" type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true" onClick={photo.close}>&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <button className={"image-card--save--modal image-card--save--"+(isDark?"dark":"light")}>
+                                    <img src={"assets/img/bookmark-"+(isDark?"dark":"light")+".png"} alt="bookmark"></img>
+                                </button>
+                                <button className={"image-card--download--modal image-card--save--"+(isDark?"dark":"light")} onClick={photo.download}>
+                                    <img src={"assets/img/download-"+(isDark?"dark":"light")+".png"} alt="download image"/>
+                                </button>
+                                <img src={photo.src} /*alt={images[key].alt}*/ className="modal--full_image"></img>
+                            </div>
+                        </div>
+                    </div>
+                </span>
+            }
+        }}
+    />;
 }
 
 export default ImageCard;
-
-
--- src/components/Paginate.jsx
+++ src/components/Paginate.jsx
@@ -8,15 +8,15 @@ function Paginate({actions}){
     return (<footer className="paginator">
         <div className={"paginate paginate--"+(isDark?"dark":"light")}>
             <button className="paginate__button" onClick={actions.prev}>&lt;</button>
-            {isNotFirstPage?<button className="paginate__button" onClick={actions.first}>1</button>:""}
-            {actions.thisPage>4?<button className="paginate__button paginate__button--dots--pc" disabled>...</button>:""}
-            {actions.thisPage-2>1?<button className="paginate__button paginate__button--pc" onClick={actions.ptp}>{actions.thisPage-2}</button>:""}
-            {(actions.thisPage-1!==1 && isNotFirstPage)?<button className="paginate__button paginate__button--pc" onClick={actions.prior}>{actions.thisPage-1}</button>:""}
+            {isNotFirstPage && <button className="paginate__button" onClick={actions.first}>1</button>}
+            {actions.thisPage>4 && <button className="paginate__button paginate__button--dots--pc" disabled>...</button>}
+            {actions.thisPage-2>1 && <button className="paginate__button paginate__button--pc" onClick={actions.ptp}>{actions.thisPage-2}</button>}
+            {(actions.thisPage-1!==1 && isNotFirstPage) && <button className="paginate__button paginate__button--pc" onClick={actions.prior}>{actions.thisPage-1}</button>}
             <button className="paginate__button" disabled><strong>{actions.thisPage}</strong></button>
-            {(actions.thisPage+1!==actions.thisLastPage && isNotLastPage)?<button className="paginate__button paginate__button--pc" onClick={actions.posterior}>{actions.thisPage+1}</button>:""}
-            {actions.thisPage+2<actions.thisLastPage?<button className="paginate__button paginate__button--pc" onClick={actions.ptTpt}>{actions.thisPage+2}</button>:""}
-            {actions.thisPage<=actions.thisLastPage-4?<button className="paginate__button paginate__button--dots--pc" disabled>...</button>:""}
-            {isNotLastPage?<button className="paginate__button" onClick={actions.lastPage}>{actions.thisLastPage}</button>:""}
+            {(actions.thisPage+1!==actions.thisLastPage && isNotLastPage) && <button className="paginate__button paginate__button--pc" onClick={actions.posterior}>{actions.thisPage+1}</button>}
+            {actions.thisPage+2<actions.thisLastPage && <button className="paginate__button paginate__button--pc" onClick={actions.ptTpt}>{actions.thisPage+2}</button>}
+            {actions.thisPage<=actions.thisLastPage-4 && <button className="paginate__button paginate__button--dots--pc" disabled>...</button>}
+            {isNotLastPage && <button className="paginate__button" onClick={actions.lastPage}>{actions.thisLastPage}</button>}
             <button className="paginate__button" onClick={actions.next}>&gt;</button>
         </div>
     </footer>);
